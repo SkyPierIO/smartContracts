a91bdb93c7d03cd6e993cda7b229caf6065f4644
mrtingalingling <ting.lik.2@gmail.com>
2023-12-27 21:59:47 -0500

    incremental update

--- contracts/nodeConfig.sol
+++ contracts/nodeConfig.sol
@@ -1,11 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract NodeConfigContract {
-    address public owner;
-    mapping(address => uint256) public paymentRecipients;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-    constructor() {
-        owner = msg.sender;
+contract NodeConfigContract is ERC721 {
+    mapping(uint256 => string) public nodeConfig; // Mapping to store the latest config of node
+
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) {}
+
+    function updateNodeConfig(
+        uint256 tokenId,
+        string memory newConfig
+    ) external {
+        require(ownerOf(tokenId) == msg.sender, "Caller is not owner");
+
+        nodeConfig[tokenId] = newConfig;
+        emit NodeConfigUpdated(tokenId, newConfig);
     }
+
+    event NodeConfigUpdated(uint256 indexed tokenId, string newConfig);
 }
