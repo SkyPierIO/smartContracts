d8d1bf7c6b34f6a4cf319181cf4c2a58b8f0beaf
Ting <ting@localhost.localdomain>
2024-04-23 15:24:46 -0400

    renaming the contract and updating the dependencies

--- contracts/hostContract.sol
+++ contracts/hostContract.sol
@@ -1,75 +1,54 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+// Useful for debugging. Remove when deploying to a live network.
+// import "hardhat/console.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract HostContract is
-    ERC721,
-    ERC721Enumerable,
-    ERC721URIStorage,
-    ERC721Burnable,
-    Ownable
-{
-    // We need the events to show in the future the list of all the nodes
-    event HostRegistered(uint256 tokenId, string nodeId);
-    event HostUnregistered(uint256 tokenId);
+contract hostContract {
+	// Events
+	event HostRegistered(address indexed host, string nodeId);
+	event HostUnregistered(address indexed host, string nodeId);
+	/**
+	 * A smart contract that stores the hosts registered on the network
+	 * @author Ting & Miguel
+	 */
+	struct NodeInfo {
+		string nodeId;
+		uint256 balance;
+		bool active;
+		uint users;
+	}
+	// State Variables
+	address public immutable owner;
+	mapping(address => NodeInfo) hostsToInfo;
 
-    uint256 private _nextTokenId;
+	// Constructor: Called once on contract deployment
+	// Check packages/hardhat/deploy/00_deploy_your_contract.ts
+	constructor(address _owner) {
+		owner = _owner;
+	}
 
-    constructor(
-        address initialOwner
-    ) ERC721("NODE", "NODE") Ownable(initialOwner) {}
+	function getHost(address host) public view returns (NodeInfo memory) {
+		return hostsToInfo[host];
+	}
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://ipfs.io/ipfs/";
-    }
+	function registerAsHost(string memory nodeId) public {
+		if(!hostsToInfo[msg.sender].active){
+			hostsToInfo[msg.sender] = NodeInfo(nodeId, 0, true, 0);
+			emit HostRegistered(msg.sender, nodeId);
+		}
+	}
 
-    function safeMint(
-        address to,
-        string memory uri,
-        string memory nodeId
-    ) public {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-        emit HostRegistered(tokenId, nodeId);
-    }
+	function unregisterAsHost(string memory nodeId) public {
+		if(hostsToInfo[msg.sender].active){
+			hostsToInfo[msg.sender].nodeId = "";
+			hostsToInfo[msg.sender].active = false;
+			emit HostUnregistered(msg.sender, nodeId);
+		}
+	}
 
-    // The following functions are overrides required by Solidity.
-
-    function _update(
-        address to,
-        uint256 tokenId,
-        address auth
-    ) internal override(ERC721, ERC721Enumerable) returns (address) {
-        return super._update(to, tokenId, auth);
-    }
-
-    function _increaseBalance(
-        address account,
-        uint128 value
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._increaseBalance(account, value);
-    }
-
-    function tokenURI(
-        uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(
-        bytes4 interfaceId
-    )
-        public
-        view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+	function useHost(address host) public {
+		hostsToInfo[host].users++;
+	}
 }
