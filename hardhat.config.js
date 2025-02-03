c2a5a42b7507ff0067dbb14b1da4c60bc3f1a0c8
mrtingalingling <ting.lik.2@gmail.com>
2025-02-03 00:29:16 +0000

    add upgradeability to hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require("@nomicfoundation/hardhat-ethers");
+require('@openzeppelin/hardhat-upgrades');
+
 module.exports = {
   solidity: "0.8.20",
 };
