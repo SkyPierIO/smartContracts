97e1ab41aaeff2ac15e9b7a7ba5ff3c87312d4a2
mrtingalingling <ting.lik.2@gmail.com>
2025-12-29 16:33:38 -0500

    Update SKP

--- contracts/product/tokens/SkypierToken.sol
+++ contracts/product/tokens/SkypierToken.sol
@@ -4,25 +4,21 @@ pragma solidity ^0.8.0;
 // The purpose of this contract is to offer ERC20 SkypierToken to our users
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
-contract SkypierToken is ERC20, AccessControl {
-    bytes32 private constant CLIENT_ROLE = keccak256("CLIENT_ROLE");
+contract SkypierToken is ERC20Upgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+    function initialize() public initializer {
+        __ERC20_init("SkypierToken", "SKP");
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
 
-    constructor() ERC20("Skypier Token", "SKY") {
-        _mint(msg.sender, 1000000 * 10**decimals());
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _mint(msg.sender, 21 * 10**9 * 10**decimals());
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender); // Critical for UUPS
     }
 
-    // function initialize() public initializer {
-    //     __ERC20_init("SkypierToken", "SKP");
-    //     _mint(msg.sender, 21 * 10**9 * 10**decimals());
-    // }
-
-
-    function mintClientToken(address to, uint256 amount) external {
-        require(hasRole(CLIENT_ROLE, msg.sender), "Not authorized");
-        _mint(to, amount);
+    function upgradeTo(address newImplementation) public virtual override onlyRole(DEFAULT_ADMIN_ROLE) {
+        _upgradeTo(newImplementation);
     }
 }
