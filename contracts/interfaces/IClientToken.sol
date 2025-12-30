828a9d20accd9a9511ad0322431ec84c3b50a0de
mrtingalingling <ting.lik.2@gmail.com>
2025-12-29 21:38:27 -0500

    update client token

--- contracts/interfaces/IClientToken.sol
+++ contracts/interfaces/IClientToken.sol
@@ -2,6 +2,11 @@
 pragma solidity ^0.8.20;
 
 interface IClientToken {
+    // --- Events ---
+    event BadgeIssued(address indexed account, uint256 indexed tokenId, uint256 amount, string metadata);
+    event BadgeRevoked(address indexed account, uint256 indexed tokenId, uint256 amount);
+
+    // --- Core Functions ---
     function issueBadge(
         address to,
         uint256 tokenId,
@@ -9,5 +14,17 @@ interface IClientToken {
         uint64 expiryTimestamp
     ) external;
 
-    uint256 CLIENT_BADGE;
+    function revokeBadge(
+        address from,
+        uint256 tokenId,
+        uint256 amount
+    ) external;
+
+    function isValidHolder(address user, uint256 tokenId) external view returns (bool);
+
+    function getExpiry(uint256 tokenId) external view returns (uint64);
+
+    // --- Token IDs (optional, but useful for clarity) ---
+    function CLIENT_BADGE() external view returns (uint256);
+    function BETA_TESTER_BADGE() external view returns (uint256);
 }
